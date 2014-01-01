b71270d6f764471d0f319142430d1fac53785d18
kumar <kumar.adventure@gmail.com>
2014-01-01 09:52:34 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kutangaza::Application.config.secret_key_base = 'f2d939af73e3abfde7b0397c04d5627c88fb938670d758453081231018f6b6fdf60e9a98993be16a122d3f2c1d7256e03cc777f61339084b8d2dc848c21e58a8'
