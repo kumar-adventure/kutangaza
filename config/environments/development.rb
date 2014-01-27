fef000a256537f0f8fcceb5f18f6f6d26cc6f201
kumar <kumar.adventure@gmail.com>
2014-01-28 01:18:59 +0530

    Some changes in for mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Kutangaza::Application.configure do
   config.assets.debug = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  #config.action_mailer.default_url_options = { :host => 'gmail.com' }
+  config.action_mailer.default_url_options = { :host => 'gmail.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
